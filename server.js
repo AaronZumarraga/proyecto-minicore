367f77c0f604b850fbcb525d320edba10b74902f
Aaron Zumarraga <aaron.zumarraga@hotmail.com>
2024-01-31 19:43:31 -0500

    conexion a la bdd

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: 'aaron',
+    database: 'calificaciones',
+};
+
+const connection = mysql.createConnection(dbConfig);
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error al conectar a la base de datos:', err);
+        return;
+    }
+    console.log('Conexión a la base de datos exitosa');
+});
