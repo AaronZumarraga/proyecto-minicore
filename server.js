bee8b27a4fac1442c5ddcacfdf99192c3e3294e0
Aaron Zumarraga <aaron.zumarraga@hotmail.com>
2024-02-02 15:35:53 -0500

    Calculo entre fechas del progreso 1

--- server.js
+++ server.js
@@ -37,6 +37,26 @@ app.get('/alumnos', (req, res) => {
     });
 });
 
+app.get('/notas/progreso1/:idEstudiante', (req, res) => {
+    const idEstudiante = req.params.idEstudiante;
+    const fechaInicio = req.query.fechaInicio || '1970-01-01';
+    const fechaFin = req.query.fechaFin || '9999-12-31';
+
+    connection.query(
+        'SELECT Nota FROM notas WHERE Id_estudiante = ? AND Fecha BETWEEN ? AND ? AND Id_nota IN (SELECT Id_nota FROM progreso1 WHERE Id_estudiante = ?)',
+        [idEstudiante, fechaInicio, fechaFin, idEstudiante],
+        (error, results) => {
+            if (error) {
+                console.error('Error al obtener las notas del primer progreso:', error);
+                res.status(500).json({ error: 'Error al obtener las notas del primer progreso' });
+            } else {
+                res.json(results);
+            }
+        }
+    );
+});
+
+
 app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`);
 });
